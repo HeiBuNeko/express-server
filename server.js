eb2592a4b1bcf1104435a519ad7a98d55adc1647
猫宫黑布 <841619762@qq.com>
2023-10-16 14:13:40 +0800

    核心概念-路由&中间件

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-const express= require('express')
+const express = require("express")
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+// get请求
+app.get("/", (req, res) => {
+  res.send("Hello World")
 })
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+// post请求
+app.post("/about", (req, res) => {
+  res.send({ name: "HeiBuNeko", age: 18 })
+})
+
+const user = express.Router() // 创建子路由
+
+user.get("/list", (req, res) => {
+  res.send("User List")
+})
+
+user.get("/:userId", (req, res) => {
+  res.send(req.params)
+})
+
+app.use("/user", user) // 注册子路由
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+  next()
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
 })
